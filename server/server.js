3a339e2acf03abfd6480861bc70d57aa014de1f8
kevin <kevin.ext@iot-valley.fr>
2018-07-24 07:47:16 +0200

    add some configurations

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 
-const publicPath = path.join(__dirname, '../public')
+const publicPath = path.join(__dirname, '../public');
+const port       = process.env.PORT || 3000;
 
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log("Listening on PORT 3000!")
+app.listen(port, () => {
+    console.log(`Listening on PORT ${port}!`)
 })
