63acb0ea9269b45cf5007c8e62ce28e680596445
kevin <kevin.ext@iot-valley.fr>
2018-07-27 07:25:02 +0200

    add new render feature for message using mustache.js

--- public/js/index.js
+++ public/js/index.js
@@ -14,26 +14,26 @@ socket.on("disconnect", function () {
 });
 
 socket.on("newMessage", function (message) {
-    var formattedTime = moment(message.createdAt).format("h:mm a");
-    console.log("newMessage", message);
-    var li = jQuery("<li></li>");
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-    jQuery("#messages").append(li);
+    var formattedTime = moment(message.createdAt).format("h:mm a");
+    var template = jQuery("#message-template").html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        text: message.text,
+        createdAt: formattedTime
+    });
+    jQuery("#messages").append(html);
 });
 
 socket.on("newLocationMessage", function (message) {
-    console.log("newMessage", message);
     var formattedTime = moment(message.createdAt).format("h:mm a");
-
-    var a = jQuery("<a target=\"_blank\">My current location</a>");
-    a.attr("href", message.url);
-    
-    var li = jQuery("<li></li>");
-    li.text(`${message.from} ${formattedTime}: `);
-    li.append(a);
-
-    jQuery("#messages").append(li);
+    var template = jQuery("#message-template").html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+    jQuery("#messages").append(html);
 });
 
 jQuery("#message-form").on("submit", function (event) {
