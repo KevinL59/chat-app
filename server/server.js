2f6489261394c4766293f797d9142bdc25737278
kevin <kevin.ext@iot-valley.fr>
2018-07-28 09:46:17 +0200

    send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -39,13 +39,20 @@ io.on("connection", (socket) => {
     });
 
     socket.on("createMessage", (message, callback) => {
-        console.log("createMessage", message);
-        io.emit("newMessage", generateMessage(message.from, message.text));
-        callback("This is from the server.");
+        var user = users.getUser(socket.id);
+
+        if (user && isRealString(message.text)) {
+            io.to(user.room).emit("newMessage", generateMessage(user.name, message.text));
+        }
+
+        callback();
     });
 
     socket.on("createLocationMessage", (location) => {
-        io.emit("newLocationMessage", generateLocationMessage("Admin", location.latitude, location.longitude));
+        var user = users.getUser(socket.id);
+        if (user) {
+            io.to(user.room).emit("newLocationMessage",  generateLocationMessage(user.name, location.latitude, location.longitude));
+        }
     });
 
     socket.on("disconnect", () => {
